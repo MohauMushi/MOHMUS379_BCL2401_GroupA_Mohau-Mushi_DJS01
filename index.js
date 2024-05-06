ee15afee3938843be3c26753557e655b751ab509
Mohau Mushi <david1mushi1@gmail.com>
2024-05-06 15:03:19 +0200

    comments for the conversion and other parts of code

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-/**
- * Debugging Guide
- * 1. Make the code more readable
- * 2. Pick up calculation errors
- * 3. Make these calculations robust such that the calculation does not give an incorrect result, it throws an error to the user if something has gone wrong (parameter used with an incorrect unit of measurement, etc)
- */
-
 // Given Parameters
 let velocityKmH = 10000; // velocity (km/h)
 let accelerationMs2 = 3; // acceleration (m/s^2)
@@ -16,22 +9,23 @@ let fuelBurnRateKgS = 0.5; // fuel burn rate (kg/s)
 // Units conversion
 let velocityMs = velocityKmH * 1000 / 3600 // Converting Km/h -> m/s
 
-//calcultes new distance
-let newDistance = distanceKm + (velocityMs * timeSeconds) 
-let newDistanceKm = newDistance / 1000;  // converting m -> km for the new distance
+//calculting new distance
+let newDistanceM = distanceKm + (velocityMs * timeSeconds) 
+let newDistanceKm = newDistanceM / 1000;  // converting m -> km for the new distance
 
 //calculating remaining fuel
 let remainingFuel = fuelKg - (fuelBurnRateKgS * timeSeconds) 
 
-// Pick up an error with how the function below is called and make it robust to such errors
+// Calculating the new velocity by adding the product of acceleration and time to the initial velocity.
 function calcNewVel(accelerationMs2, velocityMs, timeSeconds){ 
   return velocityMs + (accelerationMs2 * timeSeconds)
 }
 
-//calculates new velocity based on acceleration
+//calculating new velocity based on acceleration
 let newVelocity = calcNewVel(accelerationMs2, velocityMs, timeSeconds) 
 let newVelocityKmH = newVelocity * (3600 / 1000); // converting m/s to km/h
 
+// Consoling the output for the corrected new velocity, distance and remaining fuel
 console.log(`Corrected New Velocity: ${newVelocityKmH} km/h`);
 console.log(`Corrected New Distance: ${newDistanceKm} km`);
 console.log(`Corrected Remaining Fuel: ${remainingFuel} kg`);
