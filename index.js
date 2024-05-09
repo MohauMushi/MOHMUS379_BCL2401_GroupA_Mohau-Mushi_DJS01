6e4de2b9557237aa7fb8b4fa3c90dea83783722c
Mohau Mushi <david1mushi1@gmail.com>
2024-05-09 16:03:42 +0200

    Updated: validating the measurement properties of various objects against expected units

--- index.js
+++ index.js
@@ -89,19 +89,12 @@ function calcNewVel(props) {
   }
 
   // this code is validating the measurement properties of various objects against expected units
-  if (velocity.measurement !== "km/h") {
-    throw new Error("Invalid measurement unit for velocity. Expected: km/h");
-  } else if (acceleration.measurement !== "m/s^2") {
-    throw new Error("Invalid measurement unit for acceleration. Expected: m/s^2");
-  } else if (timeSeconds.measurement !== "s") {
-    throw new Error("Invalid measurement unit for time. Expected: s");
-  } else if (distance.measurement !== "km") {
-    throw new Error("Invalid measurement unit for distance. Expected: km");
-  } else if (fuel.measurement !== "kg") {
-    throw new Error("Invalid measurement unit for fuel. Expected: kg");
-  } else if (fuelBurnRate.measurement !== "kg/s") {
-    throw new Error("Invalid measurement unit for fuel burn rate. Expected: kg/s");
-  }
+  if (velocity.measurement !== "km/h") throw new Error("Invalid measurement unit for velocity. Expected: km/h");
+  if (acceleration.measurement !== "m/s^2") throw new Error("Invalid measurement unit for acceleration. Expected: m/s^2");
+  if (timeSeconds.measurement !== "s") throw new Error("Invalid measurement unit for time. Expected: s");
+  if (distance.measurement !== "km") throw new Error("Invalid measurement unit for distance. Expected: km");
+  if (fuel.measurement !== "kg") throw new Error("Invalid measurement unit for fuel. Expected: kg");
+  if (fuelBurnRate.measurement !== "kg/s") throw new Error("Invalid measurement unit for fuel burn rate. Expected: kg/s");
 
   // Calculate the new velocity
   const newVelocity = velocity.value + (acceleration.value * timeSeconds.value) * conversionRate;
